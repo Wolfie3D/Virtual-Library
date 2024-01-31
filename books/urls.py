f7ad8e7908883688253fbed467c04c457ca23bff
Chris Kamau <kwangechi3@gmail.com>
2024-01-31 08:42:49 -0800

    updated the db with date and improve the GET /books/ and GET /authors/ API end points

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home/', views.home, name='home'),
     path('books/', views.books, name='books'),
+    path('authors/', views.authors, name='authors'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Book, Author
+
+
+def home(request):
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render())
 
 
 def books(request):
-    template = loader.get_template('index.html')
+    available_books = Book.objects.all().values()
+    template = loader.get_template('books.html')
+    context = {
+    'available_books': available_books, 
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+def authors(request):
+    template = loader.get_template('authors.html')
     return HttpResponse(template.render())
