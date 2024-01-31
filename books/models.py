c030f823168d97d52ba001b43d65ae99f2b84eec
Chris Kamau <kwangechi3@gmail.com>
2024-01-31 10:18:18 -0800

    updated the models and the data for demo

--- books/models.py
+++ books/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 
-
 class Author(models.Model):
+    author_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     gender = models.CharField(max_length=10)
     age = models.IntegerField()
     country = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
+    
+    def __repr__(self):
+        return f"<Author {self.author_id}: {self.name}>"
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    
+    def __repr__(self):
+        return f"<book {self.book_id}: {self.title}>"
